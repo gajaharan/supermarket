52c6df9b64e93431b075074aa468321605201c90
gajaharan <gajaharan@gmail.com>
2021-08-04 17:37:42 +0100

    Add ThreeGetOneFree Test with three same items scanned return savings.

--- src/main/kotlin/com/supermarket/gaj/promotion/BuyThreeGetOneFree.kt
+++ src/main/kotlin/com/supermarket/gaj/promotion/BuyThreeGetOneFree.kt
@@ -5,6 +5,16 @@ import java.math.BigDecimal
 
 class BuyThreeGetOneFree {
     fun apply(items: List<Item>): BigDecimal {
-        return BigDecimal.ZERO.setScale(2)
+        if (items.isEmpty()) {
+            return BigDecimal.ZERO.setScale(2)
+        }
+
+        val unitPrice = items.first().unitPrice()
+        val packNumber = items.size / MULTI_BUY_QUANITY
+        return unitPrice.multiply(BigDecimal(packNumber))
+    }
+
+    companion object {
+        const val MULTI_BUY_QUANITY = 2
     }
 }
