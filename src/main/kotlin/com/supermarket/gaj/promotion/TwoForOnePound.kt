92ab3665b4da222d204db38d93d3a3ee8789b05a
gajaharan <gajaharan@gmail.com>
2021-08-04 16:55:03 +0100

    Add TwoForOnePound Test with two same items scanned return savings

--- src/main/kotlin/com/supermarket/gaj/promotion/TwoForOnePound.kt
+++ src/main/kotlin/com/supermarket/gaj/promotion/TwoForOnePound.kt
@@ -4,5 +4,9 @@ import com.supermarket.gaj.item.Item
 import java.math.BigDecimal
 
 class TwoForOnePound {
-    fun apply(items: List<Item>) = BigDecimal.ZERO
+    fun apply(items: List<Item>) =
+        items[0].unitPrice()
+            .multiply(BigDecimal(2))
+            .subtract(BigDecimal("1.00"))
+
 }
--- src/test/kotlin/com/supermarket/gaj/promotion/TwoForOnePoundTest.kt
+++ src/test/kotlin/com/supermarket/gaj/promotion/TwoForOnePoundTest.kt
@@ -1,5 +1,7 @@
 package com.supermarket.gaj.promotion
 
+import com.supermarket.gaj.item.SKU
+import com.supermarket.gaj.item.UnitItem
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import java.math.BigDecimal
@@ -10,6 +12,16 @@ internal class TwoForOnePoundTest {
     @Test
     fun `apply promotion when zero items scanned then should return no savings`() {
         val actualSavings = twoForOnePound.apply(emptyList())
-        Assertions.assertEquals(BigDecimal(0.00), actualSavings)
+        Assertions.assertEquals(BigDecimal("0.00"), actualSavings)
+    }
+
+    @Test
+    fun `apply promotion when two same items scanned then should return savings`() {
+        val items = listOf(
+            UnitItem(SKU.B, BigDecimal("0.60")),
+            UnitItem(SKU.B, BigDecimal("0.60"))
+        )
+        val actualSavings = twoForOnePound.apply(items)
+        Assertions.assertEquals(BigDecimal("0.20"), actualSavings)
     }
 }
