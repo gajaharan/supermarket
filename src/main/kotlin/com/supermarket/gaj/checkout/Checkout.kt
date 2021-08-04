daafbb5f48b68e64a52c9c4eecd95d1382f63d60
gajaharan <gajaharan@gmail.com>
2021-08-04 16:35:50 +0100

    Add test and implement for checkout when two items is scanned. Scan method implemented. Had to change BigDecimal to accept string.

--- src/main/kotlin/com/supermarket/gaj/checkout/Checkout.kt
+++ src/main/kotlin/com/supermarket/gaj/checkout/Checkout.kt
@@ -2,10 +2,16 @@ package com.supermarket.gaj.checkout
 
 import com.supermarket.gaj.item.Item
 import java.math.BigDecimal
+import java.math.RoundingMode
 
-class Checkout(private val items: List<Item>) {
+class Checkout(
+    private val items: MutableList<Item> = mutableListOf()
+) {
+    fun scan(item: Item) = items.add(item)
+
     fun total(): BigDecimal = items
         .stream()
         .map { it.unitPrice() }
         .reduce(BigDecimal.ZERO, BigDecimal::add)
+        .setScale(2, RoundingMode.HALF_UP)
 }
--- src/test/kotlin/com/supermarket/gaj/CheckoutTest.kt
+++ src/test/kotlin/com/supermarket/gaj/CheckoutTest.kt
@@ -10,18 +10,31 @@ import java.math.BigDecimal
 internal class CheckoutTest {
     @Test
     fun `checkout provides it's total value zero when containing no items`() {
-        val expectedTotal = 0;
-        val checkout = Checkout(listOf())
+        val expectedTotal = "0.00";
+        val checkout = Checkout()
         assertEquals(BigDecimal(expectedTotal), checkout.total())
     }
 
     @Test
-    fun `checkout provides it's total value zero when containing a single item`() {
-        val expectedTotal = 0.50;
+    fun `checkout provides it's total value when containing a single item`() {
+        val expectedTotal = "0.50";
         val items = listOf(
-            UnitItem(SKU.A, BigDecimal(0.50))
+            UnitItem(SKU.A, BigDecimal("0.50"))
         )
-        val checkout = Checkout(items)
+        val checkout = Checkout()
+        items.forEach { checkout.scan(it) }
+        assertEquals(BigDecimal(expectedTotal), checkout.total())
+    }
+
+    @Test
+    fun `checkout provides it's total value when containing two items`() {
+        val expectedTotal = "1.10";
+        val items = listOf(
+            UnitItem(SKU.A, BigDecimal("0.50")),
+            UnitItem(SKU.B, BigDecimal("0.60"))
+        )
+        val checkout = Checkout()
+        items.forEach { checkout.scan(it) }
         assertEquals(BigDecimal(expectedTotal), checkout.total())
     }
 }
